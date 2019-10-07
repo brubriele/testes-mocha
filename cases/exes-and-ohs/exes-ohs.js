56e6f9f66106054f9a9df1962026f0b836c8752c
Bruna Gabriele Paula <bgabriel@everis.com>
2019-10-07 17:39:14 -0300

    [exes-and-ohs] - cobertura para elementos distintos de x ou o

---
+++ cases/exes-and-ohs/exes-ohs.js
@@ -0,0 +1,15 @@
+function XO(str) {
+    for (let el in str) {
+        let re = new RegExp(str[el], "gi");
+        // if (str.match(re).length % 2 != 0) {
+        //     return str.length / 2 === str.match(re).length ? true : false
+            
+        // }
+        return str.substr(str.match(re).length === 1)
+
+    }
+}
+
+console.log(XO('Huhum'))
+
+module.exports = XO
