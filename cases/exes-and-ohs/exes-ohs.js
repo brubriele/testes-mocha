3cec1d25dfdb333cc8efef052a3b3845467280d0
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-10-10 05:49:48 -0300

    Other solutions in comment

--- cases/exes-and-ohs/exes-ohs.js
+++ cases/exes-and-ohs/exes-ohs.js
@@ -12,4 +12,27 @@ console.log(XO('xxoxBbooddoooox'))
 
 module.exports = XO
 
+// function XO(str) {
+//     for (let el in str) {
+//         let re = new RegExp(str[el], "gi");
+//         if (str.match(re).length % 2 != 0) {
+//             return str.length / 2 === str.match(re).length ? true : false
+            
+//         }
+//         return str.substr(str.match(re).length === 1)
+
+//     }
+// }
+
+
+// function charTally(input) {
+//     return input.split('').reduce((acc, char) => {
+//       acc[char] = (acc[char] || 0) + 1;
+//       return acc;
+//     }, {});
+//   }
+// console.log(XO('Huhum'))
+
+module.exports = XO
+
 // https://www.codewars.com/kata/exes-and-ohs/train/javascript
