0ee4b2d486c4b3e27e5c513fafb77aa8558e497c
Bruna Gabriele Paula <bgabriel@everis.com>
2019-10-10 16:58:11 -0300

    work on reduce empty array in string format

--- cases/exes-and-ohs/exes-ohs.js
+++ cases/exes-and-ohs/exes-ohs.js
@@ -1,23 +1,30 @@
-function XO(str) {
- 
-        return str.split("").sort().join("").match(/(.)\1+/gi).map((v, i) => {
-            return v
-        });
-  
+// function XO(str) {
+//     return str.toLowerCase().split('').reduce((acc, char) => {
+//         acc[char] = (acc[char] || 0) + 1;
+//         return  acc
+//     }, []);
+// }
 
+const XO = (str) => {
+    return str.toLowerCase().split('').reduce((acc, char) => {
+        acc[char] = (acc[char] || 0) + 1;
+        return  acc
+    }, []);
 }
 
 
-console.log(XO('xxoxBbooddoooox'))
 
-module.exports = XO
+
+console.log(XO('XoXXXoo'))
+
+// module.exports = XO
 
 // function XO(str) {
 //     for (let el in str) {
 //         let re = new RegExp(str[el], "gi");
 //         if (str.match(re).length % 2 != 0) {
 //             return str.length / 2 === str.match(re).length ? true : false
-            
+
 //         }
 //         return str.substr(str.match(re).length === 1)
 
@@ -36,3 +43,12 @@ module.exports = XO
 module.exports = XO
 
 // https://www.codewars.com/kata/exes-and-ohs/train/javascript
+
+// function XO(str) {
+
+//     return str.split("").sort().join("").match(/(.)\1+/gi).map((v, i) => {
+//         return v
+//     });
+
+
+// }
