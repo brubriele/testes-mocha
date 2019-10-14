bf54c79c7163bdebf25b57d4121dd3cce581606b
Bruna Gabriele Paula <bgabriel@everis.com>
2019-10-14 13:24:16 -0300

    Finaliza desafio, rever bug filter of null in codewars

--- cases/exes-and-ohs/exes-ohs.js
+++ cases/exes-and-ohs/exes-ohs.js
@@ -1,54 +1,18 @@
-// function XO(str) {
-//     return str.toLowerCase().split('').reduce((acc, char) => {
-//         acc[char] = (acc[char] || 0) + 1;
-//         return  acc
-//     }, []);
-// }
-
-const XO = (str) => {
-    return str.toLowerCase().split('').reduce((acc, char) => {
-        acc[char] = (acc[char] || 0) + 1;
-        return  acc
-    }, []);
+function XO(str) {
+    const result = [];
+    const testString = str.toLowerCase();
+    const strArr = testString.split("").sort().join("").match(/(.)\1+/g);
+    if (testString === 'ox' || testString === 'xo') {
+        return true
+    } else {
+        let x = strArr.filter((v) => {
+            return v.length % 2 === 0, v
+        })
+        return result.includes(false) || (result.length - 1) < 2 ? false : true
+    }
 }
 
+console.log(XO('XO'))
 
-
-
-console.log(XO('XoXXXoo'))
-
-// module.exports = XO
-
-// function XO(str) {
-//     for (let el in str) {
-//         let re = new RegExp(str[el], "gi");
-//         if (str.match(re).length % 2 != 0) {
-//             return str.length / 2 === str.match(re).length ? true : false
-
-//         }
-//         return str.substr(str.match(re).length === 1)
-
-//     }
-// }
-
-
-// function charTally(input) {
-//     return input.split('').reduce((acc, char) => {
-//       acc[char] = (acc[char] || 0) + 1;
-//       return acc;
-//     }, {});
-//   }
-// console.log(XO('Huhum'))
 
 module.exports = XO
-
-// https://www.codewars.com/kata/exes-and-ohs/train/javascript
-
-// function XO(str) {
-
-//     return str.split("").sort().join("").match(/(.)\1+/gi).map((v, i) => {
-//         return v
-//     });
-
-
-// }
