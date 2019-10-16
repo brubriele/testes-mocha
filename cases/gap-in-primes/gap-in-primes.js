49f1279f76bfddfb16761deca8ff5b2b15ee00bd
Bruna Gabriele Paula <bgabriel@everis.com>
2019-10-16 17:47:29 -0300

    procurar solucao para cobertura de null

--- cases/gap-in-primes/gap-in-primes.js
+++ cases/gap-in-primes/gap-in-primes.js
@@ -1,24 +1,20 @@
-function gap (g, n, m) {
-    //Construct array inital/final
+function gap(g, n, m) {
     let arr = new Array(m + 1).fill(m)
     let x = arr.map((v, i) => {
         return v, i
     }).slice(n)
-    // console.log(x)
-
-    //To verify primes
-    // console.log(x)
-    // return x.map((v, i) => {
-    //     // return v % 2 === 0
-    //     return Math.sqrt(v)
-    // })
-
-    return x.filter((number) => {
-        for (let i = 2; i <= Math.sqrt(number); i++){
+    let newArr = x.filter((number) => {
+        for (let i = 2; i <= Math.sqrt(number); i++) {
             if (number % i === 0) return false;
         }
         return true
     })
+    let diff = newArr.slice(1).filter(function (n, i) {
+        return n - newArr[i] === g
+    })[0]
+    const flatten = (arr, depth = 1) =>
+        arr.reduce((a, v) => a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1) : v), []);
+    return flatten([diff - g, diff])
 }
-console.log(gap(2, 10, 21))
-
+console.log(gap(6, 100, 110))
+module.exports = gap
