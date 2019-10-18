7ae0f1151986a330d3418cafbb606c70dd6eb260
Bruna Gabriele Paula <bgabriel@everis.com>
2019-10-18 17:26:44 -0300

    continua desafio, buscar solução para diminuir slice

--- cases/gap-in-primes/gap-in-primes.js
+++ cases/gap-in-primes/gap-in-primes.js
@@ -1,11 +1,10 @@
 console.time("gap");
 function gap(g, n, m) {
     const isPrime = num => ![...Array(num).keys()].slice(2).map(i => !(num%i)).includes(true) && ![0,1].includes(num)
-    let newM = m > 500 ? m / 5 : m
-    const arr = [...Array(newM)].map((v, i) => {
+    // console.log(m.toString().length)
+    const arr = [...Array(m.toString().length > 3 ? m/10 : m)].map((v, i) => {
         return v, i
     }).slice(n).filter((number) => isPrime(number))
-    console.log(arr)
     const diff = arr.slice(1).filter((n, i) => {
         return n - arr[i] === g
     })[0]
