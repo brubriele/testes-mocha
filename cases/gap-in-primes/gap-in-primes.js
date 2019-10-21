8c6ea5bff982e5e27e600ea7925e30d688c60801
Bruna Gabriele Paula <bgabriel@everis.com>
2019-10-21 16:58:31 -0300

    Continua desafio, buscar solução para timeout

--- cases/gap-in-primes/gap-in-primes.js
+++ cases/gap-in-primes/gap-in-primes.js
@@ -1,8 +1,7 @@
 console.time("gap");
 function gap(g, n, m) {
     const isPrime = num => ![...Array(num).keys()].slice(2).map(i => !(num%i)).includes(true) && ![0,1].includes(num)
-    // console.log(m.toString().length)
-    const arr = [...Array(m.toString().length > 3 ? m/10 : m)].map((v, i) => {
+    const arr = [...Array(m)].map((v, i) => {
         return v, i
     }).slice(n).filter((number) => isPrime(number))
     const diff = arr.slice(1).filter((n, i) => {
@@ -10,6 +9,8 @@ function gap(g, n, m) {
     })[0]
     return [diff -g, diff].includes(NaN) ? null : [diff -g, diff] 
 }
-console.log(gap(2, 1, 50))
+console.log(gap(2, 250, 3010))
+// console.log(gap(4, 100, 110))
+
 console.timeEnd("gap");
 module.exports = gap
